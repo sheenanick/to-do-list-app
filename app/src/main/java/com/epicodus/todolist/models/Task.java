6a5c04799c61cb7417f4c994c4d3981f0e8760e5
Sheena Nickerson <sheenanick@gmail.com>
2016-12-12 13:07:00 -0800

    add Task model

---
+++ app/src/main/java/com/epicodus/todolist/models/Task.java
@@ -0,0 +1,29 @@
+package com.epicodus.todolist.models;
+
+public class Task {
+    private String description;
+    private boolean isComplete;
+
+    public Task() {}
+
+    public Task(String description) {
+        this.description = description;
+        this.isComplete = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+}
