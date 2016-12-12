1f4de38aeec5cd9e8ff45982d5b60752e18f9c85
Sheena Nickerson <sheenanick@gmail.com>
2016-12-12 14:28:50 -0800

    display tasks in Recycler View with Firebase Adapter

--- app/src/main/java/com/epicodus/todolist/adapters/FirebaseTaskListAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.epicodus.todolist.adapters;
-
-/**
- * Created by Guest on 12/12/16.
- */
-public class FirebaseTaskListAdapter {
-}
--- app/src/main/java/com/epicodus/todolist/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/todolist/ui/MainActivity.java
@@ -2,12 +2,16 @@ package com.epicodus.todolist.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.epicodus.todolist.R;
+import com.epicodus.todolist.adapters.FirebaseTaskViewHolder;
 import com.epicodus.todolist.models.Task;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -17,6 +21,10 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.newTaskEditText) EditText mNewTask;
     @Bind(R.id.addButton) Button mAddButton;
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    private DatabaseReference mTaskReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
 
         mAddButton.setOnClickListener(this);
+
+        mTaskReference = FirebaseDatabase.getInstance().getReference("tasks");
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Task, FirebaseTaskViewHolder>(Task.class, R.layout.task_list_item, FirebaseTaskViewHolder.class, mTaskReference) {
+            @Override
+            protected void populateViewHolder(FirebaseTaskViewHolder viewHolder, Task model, int position) {
+                viewHolder.bindTask(model);
+            }
+        };
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
     }
 
     @Override
